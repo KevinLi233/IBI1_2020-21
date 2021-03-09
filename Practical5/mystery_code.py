bfa829887035f86b9341608cde4210203199afcd
3200110563bit <3200110563@zju.edu.cn>
2021-03-09 17:35:46 +0800

    ready to submit

---
+++ Practical5/mystery_code.py
@@ -0,0 +1,21 @@
+# What does this piece of code do?
+# Answer:get a random number from 1 to 100 until the random number is less than 50, and output it.
+
+# Import libraries
+# randint allows drawing a random number, 
+# e.g. randint(1,5) draws a number between 1 and 5
+from random import randint
+
+# ceil takes the ceiling of a number, i.e. the next higher integer.
+# e.g. ceil(4.2)=5
+from math import ceil
+
+
+p=False
+while p==False:
+	p = True
+	n = randint(1,100)
+	if n > 50:
+		p = False
+
+print(n)
