1a6e8f15202935655fe087a08e9800b879ac84a5
3200110563bit <3200110563@zju.edu.cn>
2021-03-09 20:44:33 +0800

    debug

--- Practical5/r_rate.py
+++ Practical5/r_rate.py
@@ -7,7 +7,7 @@ r = float(input("Please enter the r number:"))
 #gain the number r
 n = 84
 #set number of students
-for i in range(1,5):
+for i in range(0,5):
 	n = n * (1 + r)
 #calculate the total number of individuals infected after 5 generations.
 #actually equal to n = n*(1+r)^5
