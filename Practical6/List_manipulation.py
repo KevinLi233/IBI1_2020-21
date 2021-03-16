7bd70d115273665796fd213a720a48128ac80de2
3200110563bit <3200110563@zju.edu.cn>
2021-03-16 10:28:54 +0800

    pseu list

--- Practical6/List_manipulation.py
+++ Practical6/List_manipulation.py
@@ -1,22 +1,35 @@
+# Import libraries
 import numpy as np
 import matplotlib.pyplot as plt 
 
+# List initialization
 gene_len = []
 exon_cot = []
 aver = []
+# Input gene lengths
+# Input data as a string
 g = input("input gene lengths:")
+# split the string into a list
 g = list(g.split(','))
+# convert data types
 for i in range(len(g)):
     gene_len.append(int(g[i]))
-
+# Input exon counts
+# Input data as a string
 e = input("input exon counts:")
+# split the string into a list
 e = list(e.split(','))
+# convert data types
 for i in range(len(e)):
     exon_cot.append(int(e[i]))
-print(gene_len)
+# get average exon length of each gene
 for i in range(len(gene_len)):
     aver.append(gene_len[i] / exon_cot[i])
-print(aver)
+# sort values for the average
+aver = sorted(aver)
+# print the sorted values for the average exon length
+print("sorted values for the average exon length:",aver)
+# make a boxplot
 plt.boxplot(
     aver,
     vert = True,
