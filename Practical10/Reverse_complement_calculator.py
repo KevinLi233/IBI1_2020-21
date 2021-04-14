66a55a643b34a2236b0e7fe72e4066ecda8f9c51
3200110563bit <3200110563@zju.edu.cn>
2021-04-15 01:57:14 +0800

    P10

---
+++ Practical10/Reverse_complement_calculator.py
@@ -0,0 +1,27 @@
+def reverse(DNAs):
+    '''
+    Input: DNAs, a string formed by ['A','a','T','t','C','c','G','g']
+	Returns a string
+    '''
+    seq = list(DNAs)
+    for i in range(len(seq)):
+        if (seq[i]=='A')or(seq[i]=='a'):
+            seq[i]='T'
+            continue
+        if (seq[i]=='C')or(seq[i]=='c'):
+            seq[i]='G'
+            continue
+        if (seq[i]=='G')or(seq[i]=='g'):
+            seq[i]='C'
+            continue
+        if (seq[i]=='T')or(seq[i]=='t'):
+            seq[i]='A'
+            continue
+    s = ''
+    for i in range(len(seq)-1,-1,-1):
+        s = s + str(seq[i])
+    print("Reverse complement:",s)
+
+
+DNAseq = input("DNA sequence:")
+reverse(DNAseq)
